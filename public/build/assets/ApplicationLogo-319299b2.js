65141b34afc3ef7dc2faa39328eb18335af54fae
Timothy Kibalama <timkibalama@gmail.com>
2023-09-17 12:34:06 +0300

    added update pages

---
+++ public/build/assets/ApplicationLogo-319299b2.js
@@ -0,0 +1,1 @@
+import{a}from"./app-2d7fc0bf.js";function s(o){return a("img",{src:"/logo.png",alt:"",className:"",...o})}export{s as A};
--- public/build/assets/ApplicationLogo-655b81cd.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./app-ccb9fa51.js";function s(o){return a("img",{src:"/logo.png",alt:"",className:"",...o})}export{s as A};
